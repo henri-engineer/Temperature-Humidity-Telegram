e1cbb09362c351a7eaf76120fb8301ef39077553
henri-engineer <rm564586@fiap.com.br>
2025-10-17 14:36:53 -0300

    Add secrets.h for sensitive configuration settings

---
+++ secrets.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// ------------------- CONFIGURAÇÕES SENSÍVEIS -------------------
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "sensor/dht22henri";
+
+#endif  // SECRETS_H
